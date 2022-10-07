a6531c381ae3ac206f63f8f9c6af1ae52bb87c06
Rhesa Davinanto <rhesa.davinanto@nadihealth.com>
2022-10-07 23:22:45 +0800

    fixing

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const socket = require("socket.io")
-const userRoutes = require('./routes/userRoutes');
-const messagesRoutes = require('./routes/messagesRoutes');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const socket = require("socket.io");
+const userRoutes = require("./routes/userRoutes");
+const messagesRoutes = require("./routes/messagesRoutes");
 
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json({ extended: false }));
 
-app.use('/api/auth', userRoutes)
-app.use('/api/messages', messagesRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messagesRoutes);
 
-mongoose.connect(process.env.MONGO_PRODUCTION_URL, {
+mongoose
+  .connect(process.env.MONGO_PRODUCTION_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Database connected')
-}).catch((err) => {
-    console.log(err.message)
-})
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+const port = process.env.PORT;
 
-const port = process.env.PORT
+app.get("/", (req, res) => {
+  res.send("API nya Rhesa");
+});
+
 const server = app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`)
-})
+  console.log(`server started at http://localhost:${port}`);
+});
 
 const io = socket(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        credential: true
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    credential: true,
+  },
+});
 
-global.onlineUsers = new Map()
+global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-    global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-        onlineUsers.set(userId, socket.id)
-    })
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
-    socket.on("send-msg", (data) => {
-        const sendUserSocket = onlineUsers.get(data.to)
-        if(sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-receive", data.message)
-        }
-    })
-})
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
+});
